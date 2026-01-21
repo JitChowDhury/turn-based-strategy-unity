be4c3425a09d5cb645a9c0d8cda2e1a2c5a34ccc
JitChowDhury <jitchowdhury8717@gmail.com>
2026-01-21 20:40:11 +0530

    Unit Selected Visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,10 +1,25 @@
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+    public event EventHandler OnSelectedUnitChanged;
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+
+    private void Awake()
+    {
+
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitACtionSystem!" + transform + "-" + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -23,10 +38,21 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (hit.collider.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
+
                 return true;
             }
         }
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
